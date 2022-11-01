456b01fc90d6f57a1046c90daabc31ae4fd2258b
Pablo España <espana.pablo95@gmail.com>
2022-11-01 12:47:15 -0500

    first commit

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 from fastapi.responses import HTMLResponse
 
 app = FastAPI()
@@ -41,4 +41,16 @@ def get_movie(id: int):
 
 @app.get('/movies/', tags=['movies'])
 def get_movies_by_category(category: str, year: int):
-    return category
+    return [ item for item in movies if item['category'] == category ]
+
+@app.post('/movies', tags=['movies'])
+def create_movie(id: int = Body(), title: str = Body(), overview:str = Body(), year:int = Body(), rating: float = Body(), category: str = Body()):
+    movies.append({
+        "id": id,
+        "title": title,
+        "overview": overview,
+        "year": year,
+        "rating": rating,
+        "category": category
+    })
+    return movies
