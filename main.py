8791d00af1871383012f94d0d0642f9e8b4efa30
Pablo España <espana.pablo95@gmail.com>
2022-11-01 11:03:51 -0500

    first commit

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 
 app = FastAPI()
+app.title = "Mi aplicación con  FastAPI"
+app.version = "0.0.1"
 
-@app.get('/')
+@app.get('/', tags=['home'])
 def message():
     return "Hello world!"
