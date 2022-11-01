d6a31348dc5d948c4a04de8659bdb1090cb4f577
Pablo España <espana.pablo95@gmail.com>
2022-11-01 11:42:27 -0500

    first commit

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 app.title = "Mi aplicación con  FastAPI"
 app.version = "0.0.1"
 
+movies = [
+    {
+		"id": 1,
+		"title": "Avatar",
+		"overview": "En un exuberante planeta llamado Pandora viven los Na'vi, seres que ...",
+		"year": "2009",
+		"rating": 7.8,
+		"category": "Acción"
+	}
+]
+
 @app.get('/', tags=['home'])
 def message():
-    return "Hello world!"
+    return HTMLResponse('<h1>Hello world</h1>')
+
+@app.get('/movies', tags=['movies'])
+def get_movies():
+    return movies
