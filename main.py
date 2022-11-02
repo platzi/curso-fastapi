f3dac405024e3513b507953e8fee63e386bbfc4a
Sacha Lifszyc <sacha.lifszyc@gmail.com>
2022-11-02 09:17:51 -0500

    my first commit

--- main.py
+++ main.py
@@ -1,7 +1,77 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
+from fastapi.responses import HTMLResponse
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
+app.title = "Mi aplicación con  FastAPI"
+app.version = "0.0.1"
 
-@app.get('/')
+class Movie(BaseModel):
+    id: Optional[int] = None
+    title: str
+    overview: str
+    year: int
+    rating: float
+    category: str
+
+movies = [
+    {
+		"id": 1,
+		"title": "Avatar",
+		"overview": "En un exuberante planeta llamado Pandora viven los Na'vi, seres que ...",
+		"year": "2009",
+		"rating": 7.8,
+		"category": "Acción"
+	},
+    {
+		"id": 2,
+		"title": "Avatar",
+		"overview": "En un exuberante planeta llamado Pandora viven los Na'vi, seres que ...",
+		"year": "2009",
+		"rating": 7.8,
+		"category": "Acción"
+	}
+]
+
+@app.get('/', tags=['home'])
 def message():
-    return "Hello world!"
+    return HTMLResponse('<h1>Hello world</h1>')
+
+@app.get('/movies', tags=['movies'])
+def get_movies():
+    return movies
+
+@app.get('/movies/{id}', tags=['movies'])
+def get_movie(id: int):
+    for item in movies:
+        if item["id"] == id:
+            return item
+    return []
+
+@app.get('/movies/', tags=['movies'])
+def get_movies_by_category(category: str, year: int):
+    return [ item for item in movies if item['category'] == category ]
+
+@app.post('/movies', tags=['movies'])
+def create_movie(movie: Movie):
+    movies.append(movie)
+    return movies
+
+@app.put('/movies/{id}', tags=['movies'])
+def update_movie(id: int, movie: Movie):
+	for item in movies:
+		if item["id"] == id:
+			item['title'] = movie.title
+			item['overview'] = movie.overview
+			item['year'] = movie.year
+			item['rating'] = movie.rating
+			item['category'] = movie.category
+			return movies
+
+@app.delete('/movies/{id}', tags=['movies'])
+def delete_movie(id: int):
+    for item in movies:
+        if item["id"] == id:
+            movies.remove(item)
+            return movies
