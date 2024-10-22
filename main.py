7f70ef16efe289e1308b35c5572a56d1056777d5
Luis Martinez <lcmartinezl05@gmail.com>
2024-10-22 18:06:59 -0500

    Add time endpoint, with a variable in the path

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import zoneinfo
+from datetime import datetime
+
 from fastapi import FastAPI
 
 
@@ -7,3 +10,20 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hola, Luis!"}
+
+
+country_timezones = {
+    "CO": "America/Bogota",
+    "MX": "America/Mexico_City",
+    "AR": "America/Argentina/Buenos_Aires",
+    "BR": "America/Sao_Paulo",
+    "PE": "America/Lima",
+}
+
+
+@app.get("/time/{iso_code}")
+async def time(iso_code: str):
+    iso = iso_code.upper()
+    timezone_str = country_timezones.get(iso)
+    tz = zoneinfo.ZoneInfo(timezone_str)
+    return {"time": datetime.now(tz)}
