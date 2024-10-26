78d8abdcbe1644362cea96298fbeef36e0e7f08a
Luis Martinez <lcmartinezl05@gmail.com>
2024-10-25 19:23:59 -0500

    Uso Básico de Middleware en FastAPI

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
+import time
 import zoneinfo
 from datetime import datetime
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from sqlmodel import select
 
 from db import SessionDep, create_all_tables
@@ -13,6 +14,16 @@ app = FastAPI(lifespan=create_all_tables)
 app.include_router(customers.router)
 app.include_router(transactions.router)
 app.include_router(plans.router)
+
+
+@app.middleware("http")
+async def log_request_time(request: Request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
+    print(f"Request: {request.url} completed in: {process_time:.4f} seconds")
+
+    return response
 
 
 @app.get("/")
@@ -30,7 +41,7 @@ country_timezones = {
 
 
 @app.get("/time/{iso_code}")
-async def time(iso_code: str):
+async def get_time_by_iso_code(iso_code: str):
     iso = iso_code.upper()
     timezone_str = country_timezones.get(iso)
     tz = zoneinfo.ZoneInfo(timezone_str)
